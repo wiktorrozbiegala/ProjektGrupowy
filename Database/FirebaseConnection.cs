9ff6f0e7f827453e46c1cc9cf51171b74883f366
uarmes <bartlomiej.semrau@gmail.com>
2018-05-12 18:45:03 +0200

    puchati
    puchati

---
+++ Database/FirebaseConnection.cs
@@ -0,0 +1,39 @@
+﻿using FireSharp;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PGRForms.Database
+{
+    public class FirebaseConnection
+    {
+        private IFirebaseClient _client;
+
+        public FirebaseConnection()
+        {
+            IFirebaseConfig config = new FirebaseConfig()
+            {
+                BasePath = "https://projektgrupowy-pg2018.firebaseio.com"
+            };
+            _client = new FirebaseClient(config);
+
+        }
+
+        public Dictionary<string, List<Measurement>> GetAllSessionsMeas()
+        {
+            FirebaseResponse response = _client.Get("networkInfo");
+            return response.ResultAs<Dictionary<string, List<Measurement>>>();
+        }
+
+        public List<Measurement> GetSingleSessionMeas(string session)
+        {
+            FirebaseResponse response = _client.Get($"networkInfo/{session}");
+            return response.ResultAs<List<Measurement>>();
+        }
+    }
+}
---
+++ Database/Measurement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PGRForms
+{
+    /// <summary>
+    /// Consists of all params from database
+    /// </summary>
+    public class Measurement
+    {
+        public string AsuLevel { get; set; }
+        public string CQI { get; set; }
+        public string CellId { get; set; }
+        public string CellIdentity { get; set; }
+        public string MobileCountryCode { get; set; }
+        public string MobileNetworkCode { get; set; }
+        public string RSRP { get; set; }
+        public string RSRQ { get; set; }
+        public string SNR { get; set; }
+        public string SignalStrengthdBm { get; set; }
+        public string TrackingAreaCode { get; set; }
+    }
+}
