15dc83f54ed20419499bcb90bbbcf332dfd16f0a
Lukasz Wieczorek <lukasz.wieczorek@aol.com>
2018-05-13 21:15:30 +0200

    app for devices with api under 26

---
+++ LTEStatus_api17+/app/src/main/java/com/example/mlody/ltestatus/NetworkParams.java
@@ -0,0 +1,34 @@
+package com.example.mlody.ltestatus;
+
+public class NetworkParams {
+    //CellIdentityLte
+    public int CellIdentity;       //getCi()
+    public int MobileCountryCode;   //getMcc()
+    public int MobileNetworkCode;   //getMnc()
+    public int CellId;          //getPci()
+    public int TrackingAreaCode;       //getTac()
+    //CellSignalStrength
+    public int AsuLevel;       //getAsuLevel()
+    public int CQI;        //getCqi()
+    public int SNR;        //getRssnr()
+    public int SignalStrengthdBm;      //getDbm()
+    public int RSRP;    //getRsrp()
+    public int RSRQ;       //getRsrq()
+
+    public NetworkParams(int CellIdentity, int MobileCountryCode, int MobileNetworkCode,
+                         int CellId, int TrackingAreaCode, int AsuLevel, int CQI, int SNR,
+                         int RSRP, int RSRQ, int SignalStrengthdBm){
+        this.CellIdentity = CellIdentity;
+        this.MobileCountryCode = MobileCountryCode;
+        this.MobileNetworkCode = MobileNetworkCode;
+        this.CellId = CellId;
+        this.TrackingAreaCode = TrackingAreaCode;
+        this.AsuLevel = AsuLevel;
+        this.CQI = CQI;
+        this.SNR = SNR;
+        this.RSRP = RSRP;
+        this.RSRQ = RSRQ;
+        this.SignalStrengthdBm = SignalStrengthdBm;
+    }
+}
+
